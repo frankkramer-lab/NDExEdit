8937d18f9a7d14e642fdc1e3636efedf2fc063d4
Simone Mayer <simone1.mayer@student.uni-augsburg.de>
2021-03-30 10:49:31 +0200

    [FIX] removed console log when rendering a network

--- src/app/components/main-graph/main-graph.component.ts
+++ src/app/components/main-graph/main-graph.component.ts
@@ -90,7 +90,7 @@ export class MainGraphComponent implements AfterViewInit, OnDestroy {
   private renderGraph(): void {
 
     this.graphService.render(this.dataService.getSelectedNetwork())
-      .then(network => console.log(network))
+      .then(network => {})
       .catch(e => console.error(e));
   }
 
